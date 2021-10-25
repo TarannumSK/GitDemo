4b8d144ef44485c0586c9e99050a3b7793a74073
TarannumSK <tarannum.sk786@gmail.com>
2021-10-25 21:41:46 +0530

    First project added for practice

---
+++ MavenBasics/src/test/java/sampleTest/MavenSample.java
@@ -0,0 +1,24 @@
+package sampleTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenSample {
+	
+	@Test
+	public void testMavenWithSelenium() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://demo.vtiger.com/vtigercrm/index.php");
+		driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+	}
+
+}
